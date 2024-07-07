2942cd1e77814a9f590d7d72c9b3ee39b51d19cb
Камнев Николай <kamnev.qa@yandex.ru>
2024-07-08 00:48:46 +0300

    Changes: 1) A fixture has been created that opens the Firefox browser + the Yandex Scooter website; 2) A data file has been created

---
+++ conftest.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+import pytest
+from data import Urls
+
+
+@pytest.fixture(scope="function")
+def driver():
+    firefox_driver = webdriver.Firefox()
+    firefox_driver.get(Urls.MAIN_PAGE_SCOOTER)
+    yield firefox_driver
+    firefox_driver.quit()
---
+++ data.py
@@ -0,0 +1,2 @@
+class Urls:
+    MAIN_PAGE_SCOOTER = 'https://qa-scooter.praktikum-services.ru/'
